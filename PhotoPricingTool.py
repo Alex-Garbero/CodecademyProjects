055f2ade43b0d5f0b0a77d2eeee4b70373df38e5
alex <144194651+Alex-Garbero@users.noreply.github.com>
2024-02-06 10:47:23 -0800

    Create PhotoPricingTool.py

---
+++ PhotoPricingTool.py
@@ -0,0 +1,17 @@
+# Portrait Photography Pricing Tool:
+
+class Welcome:
+    def __init__(self):
+        welcome_message = "Welcome to Alex's Photography Pricing tool!\n\n"
+        print(welcome_message)
+
+class Data:
+    def __init__(self):
+        pass
+
+Welcome.welcome()
+
+# How many hours?
+# How many people?
+# What gear is needed?
+# How many photos? (Calculate hours of editing with this)
